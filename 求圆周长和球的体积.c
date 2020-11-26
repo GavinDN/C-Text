4b83e5c8d830d5f23a631aef5ed5972c86aaabe6
Gavin <370809296@qq.com>
2020-11-26 13:27:27 +0800

    简单的C语言程序

--- 求圆周长和球的体积.c
+++ 求圆周长和球的体积.c
@@ -5,15 +5,15 @@
 int main()
 {
 	int R;
-	double l,v;
+	double c,v;
 	
 	printf("请输入半径：\n"); 
 	scanf("%d",&R);
 	
-	l = 2*PI*R;
+	c = 2*PI*R;
 	v = (4/3)*PI*(R*R*R);
 	
-	printf("圈的周长：%.4lf\n球的体积：%.4lf\n",l,v);
+	printf("圈的周长：%.4lf\n球的体积：%.4lf\n",c,v);
 	
    return 0;
 }
