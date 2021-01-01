78eae1f8b405306f2fa6d32588b88c364a5ba3cc
Gavin <370809296@qq.com>
2021-01-01 22:41:28 +0800

    搞怪表情2

---
+++ 搞怪表情2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("      ___   \n");
+	printf("    **   ** \n");
+	printf("  **       **\n");
+	printf(" **  ->  -> **\n");
+	printf(" **     _   **\n");
+	printf("  **       **\n");
+	printf("    **   **\n");
+	
+	return 0; 
+} 
