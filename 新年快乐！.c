52ea42b5cc7d0febc7b4b198764956477e446968
Gavin <370809296@qq.com>
2021-02-12 23:18:08 +0800

    新年快乐！！！

---
+++ 新年快乐！.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char Han[10] = {"新年快乐！"};//定义数组并存放字符串 
+	char i[5] = {"Happy"};//定义数组并存放字符串 
+	char j[11] = {"Chinese New"};//定义数组并存放字符串
+	char k[5] = {"Year"};//定义数组并存放字符串
+	printf("%s\n%s %s %s",Han,i,j,k);//打印字符串 
+	
+	return 0;
+} 
