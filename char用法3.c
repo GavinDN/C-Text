9968f7c445c9f7ad7279884b9e99f3e42693585a
Gavin <370809296@qq.com>
2021-02-17 23:38:24 +0800

    灵活运用ASCII码表所对应的十进制数，来进行简单的逻辑判断

---
+++ char用法3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	char test;//定义一个字符型变量 i 
+
+	printf("请问您是男生还是女生？（请输入M/F）\n");
+	scanf("%c",&test); 
+	 
+	switch(test)
+	{
+		case 77: printf("原来你是男生啊！");break;
+		case 70: printf("原来你是女生啊！");break;
+		default : printf("输入错误！");
+	} 
+
+		
+	return 0;
+}
